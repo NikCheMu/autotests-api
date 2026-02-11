2c85ad89e2778ca4ab264877a819f7203d344ac2
NikCheMu <nikchemnii@yandex.ru>
2026-02-11 21:50:38 +0300

    Написание API автотестов используя Pytest. Практика написания API автотестов.

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -21,3 +21,17 @@ def assert_equal(actual: Any, expected: Any, name: str):
     :raises AssertionError: Если фактическое значение не равно ожидаемому.
     """
     assert actual == expected, f"Incorrect value of {name}. Actual {actual}, Expected {expected}"
+
+
+def assert_is_true(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение является истинным.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение ложно.
+    """
+    assert actual, (
+        f'Incorrect value: "{name}". '
+        f'Expected true value but got: {actual}'
+    )
