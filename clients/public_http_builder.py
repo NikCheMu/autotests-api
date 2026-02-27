2d2fafff6f07841eed6760ee20be5483379f5b9b
NikCheMu <nikchemnii@yandex.ru>
2026-02-27 12:32:01 +0300

    Настройки автотестов с Pydantic

--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,6 +1,7 @@
 from httpx import Client
 
 from clients.event_hooks import curl_event_hook
+from config import settings
 
 
 def get_public_http_client() -> Client:
@@ -11,6 +12,6 @@ def get_public_http_client() -> Client:
     """
 
     return Client(
-        timeout=100,
-        base_url="http://127.0.0.1:8000",
+        timeout=settings.http_client.timeout,
+        base_url=settings.http_client.client_url,
         event_hooks={"request": [curl_event_hook]})
